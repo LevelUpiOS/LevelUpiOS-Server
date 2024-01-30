b502ab7794dfe0665afe2d07748f25c5cc8818a8
Yeongwoo Kim <yw1221@g.skku.edu>
2024-01-30 21:29:11 +0900

    docs(gradle): all-open 플러그인 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,12 @@ plugins {
     kotlin("plugin.jpa") version "1.9.22"
 }
 
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.MappedSuperClass")
+}
+
 group = "com.levelup"
 version = "0.0.1-SNAPSHOT"
 
