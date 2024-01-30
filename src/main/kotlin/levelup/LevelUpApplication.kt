adce70c95c1bf2bf9aee70d9aef461f749ac95c8
Yeongwoo Kim <yw1221@g.skku.edu>
2024-01-30 21:25:21 +0900

    Init project

---
+++ src/main/kotlin/levelup/LevelUpApplication.kt
@@ -0,0 +1,11 @@
+package levelup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LevelUpApplication
+
+fun main(args: Array<String>) {
+    runApplication<LevelUpApplication>(*args)
+}
---
+++ src/test/kotlin/levelup/LevelUpApplicationTests.kt
@@ -0,0 +1,13 @@
+package levelup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LevelUpApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
