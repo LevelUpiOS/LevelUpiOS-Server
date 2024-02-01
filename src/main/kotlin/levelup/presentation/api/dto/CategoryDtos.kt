7312f1fc955fc87d406a6e0f81c5679a2abba93a
Yeongwoo Kim <yw1221@g.skku.edu>
2024-02-01 20:44:02 +0900

    fix(dto): API response 수정

--- src/main/kotlin/levelup/presentation/api/dto/CategoryDtos.kt
+++ src/main/kotlin/levelup/presentation/api/dto/CategoryDtos.kt
@@ -1,12 +1,21 @@
 package levelup.presentation.api.dto
 
 import levelup.domain.Category
-import levelup.domain.Exam
 
 data class CategoryListResponse(private val _categories: List<Category>) {
-    val categories: List<CategoryResponse> = _categories.map { CategoryResponse(it.exams) }
+    val categories: List<CategoryResponse> = _categories.map { CategoryResponse(it) }
 }
 
-data class CategoryResponse(private val _exams: List<Exam>) {
-    val exams: List<ExamResponse> = _exams.map { ExamResponse(it.id, it.name) }
+data class CategoryResponse(
+    val id: Long,
+    val name: String,
+    val description: String,
+    val exams: List<ExamResponse>
+) {
+    constructor(category: Category) : this(
+        id = category.id,
+        name = category.name,
+        description = category.description,
+        exams = category.exams.map { ExamResponse(it.id, it.name) }
+    )
 }
