58d2f2b4d0485c722e914ccaf9a16c00332b8d02
Yeongwoo Kim <yw1221@g.skku.edu>
2024-02-08 16:26:05 +0900

    fix(category): 시험이 없는 카테고리를 불러오지 않는 문제 해결

--- src/main/kotlin/levelup/domain/CategoryRepository.kt
+++ src/main/kotlin/levelup/domain/CategoryRepository.kt
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.Query
 import support.EntityRepository
 
 interface CategoryRepository : EntityRepository<Category, Long> {
-    @Query("SELECT DISTINCT c FROM Category c JOIN FETCH c.exams")
+    @Query("SELECT DISTINCT c FROM Category c LEFT JOIN FETCH c.exams")
     fun findAllWithExams(): List<Category>
 }
